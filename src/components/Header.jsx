1b6a2d35a7af91b5083832276b830c52fb030fbc
Mehul Puri <76563079+mehulpuri@users.noreply.github.com>
2022-03-16 20:10:34 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LightBulbIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <LightBulbIcon fontSize="large" />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
